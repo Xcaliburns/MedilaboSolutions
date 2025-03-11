69180f478fe2439e77835b9b05ff079264de6e13
David Abruzzo <davidabruzzogames@gmail.com>
2025-03-11 10:42:02 +0100

    added delete notes , fixed patient details reload

--- PatientNotes/Services/NotesService.cs
+++ PatientNotes/Services/NotesService.cs
@@ -17,18 +17,23 @@ public class NotesService : INotesService
     //toutes les notes
     public async Task<List<Note>> GetAsync() =>
         await _notesRepository.GetAsync();
+
     // une note
     public async Task<Note> GetAsync(string id) =>
         await _notesRepository.GetAsync(id);
+
     // les notes d'un patient
     public async Task<List<Note>> GetByPatientId(int patientId) =>
         await _notesRepository.GetByPatientId(patientId);
+
     // ajouter une note
     public async Task CreateAsync(Note newNote) =>
         await _notesRepository.CreateAsync(newNote);
+
     // modifier une note
     public async Task UpdateAsync(string id, Note updatedNote) =>
         await _notesRepository.UpdateAsync(id, updatedNote);
+
     // supprimer une note
     public async Task RemoveAsync(string id) =>
         await _notesRepository.RemoveAsync(id);
